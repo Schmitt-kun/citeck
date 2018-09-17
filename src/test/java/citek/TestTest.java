0c9dcdc6fb23f3fa6e8cdc2d94afcf0cd7426ee5
IgorKuznetsov <igor@it32.yuterra.ru>
2018-09-17 21:15:09 +0300

    Maven project initiation

---
+++ src/test/java/citek/TestTest.java
@@ -0,0 +1,39 @@
+package citek;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kuznetsov Igor
+ */
+public class TestTest {
+    
+    public TestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void test() {
+        System.out.println("This is test");
+    }
+}
