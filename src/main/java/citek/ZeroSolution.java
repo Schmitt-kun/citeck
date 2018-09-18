5c36940966f3d35f6162b4177c115176efc4759a
IgorKuznetsov <ingvarrschmitt@gmail.com>
2018-09-18 11:25:16 +0300

    Task 3

---
+++ src/main/java/citek/ZeroSolution.java
@@ -0,0 +1,28 @@
+package citek;
+
+/**
+ *
+ * @author Kuznetsov Igor
+ */
+public class ZeroSolution {
+    
+    /**
+     * Не совсем верное решение для случая, если число десятичное.
+     * В используемых методах String есть циклы.
+     * 
+     * @param i
+     * @return 
+     */
+    public int solution(int i) {
+        String num = String.valueOf(i);
+        if(num.lastIndexOf("0") == -1)
+            return i;
+        int index = num.length() - num.lastIndexOf("0");
+        return i + (int)Math.pow(10, index - 1) * (i >= 0 ? 1 : -1);
+    }
+    
+    // Решение для двоичного представления числа.
+    public int binarySolution(int i) {
+        return (i + 1) | i;
+    }
+}
---
+++ src/test/java/citek/ZeroSolutionTest.java
@@ -0,0 +1,93 @@
+/*
+ * 
+ */
+package citek;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kuznetsov Igor
+ */
+public class ZeroSolutionTest {
+    
+    ZeroSolution s = new ZeroSolution();
+    
+    public ZeroSolutionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void test() {
+        int i = 0;
+        assertTrue(1 == s.solution(i));
+    }
+    
+    @Test
+    public void test1() {
+        int i = 10;
+        assertTrue(11 == s.solution(i));
+    }
+    
+    @Test
+    public void test2() {
+        int i = 2;
+        assertTrue(2 == s.solution(i));
+    }
+    
+    @Test
+    public void test3() {
+        int i = 100;
+        assertTrue(101 == s.solution(i));
+    }
+    
+    @Test
+    public void test4() {
+        int i = -100;
+        assertTrue(-101 == s.solution(i));
+    }
+    
+    @Test
+    public void test5() {
+        int i = 101;
+        assertTrue(111 == s.solution(i));
+    }
+    
+    @Test
+    public void binaryTest() {
+        int i = 0b0;
+        assertTrue(0b1 == s.binarySolution(i));
+    }
+    
+    @Test
+    public void binaryTest1() {
+        int i = 0b10;
+        assertTrue(0b11 == s.binarySolution(i));
+    }
+    
+    @Test
+    public void binaryTest2() {
+        int i = 0b101;
+        assertTrue(0b111 == s.binarySolution(i));
+    }
+}
